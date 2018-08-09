e334ebb94dfca4b9d1296075f4859e5f9f1d8982
Robin Broda <robin@broda.me>
2018-08-09 02:05:37 +0200

    Add Cocktail Shaker Sort

--- src/algo.cpp
+++ src/algo.cpp
@@ -33,6 +33,42 @@ namespace algo {
     }
   };
 
+  class CocktailShakerSort : public IAlgo {
+  public:
+    void run(){
+      bool swapped = true;
+      while(swapped){
+        { // forwards
+          swapped = false;
+          for(size_t i = 1; i < target.size(); i++){
+            if(target[i-1] > target[i]){
+              swap(target[i-1], target[i]);
+              swapped = true;
+            }
+
+            this_thread::sleep_for(chrono::microseconds(delay));
+            if(!running) return;
+          }
+        }
+
+        if(!swapped) return;
+
+        { // backwards
+          swapped = false;
+          for(size_t i = target.size()-2; i > 0; i--){
+            if(target[i] > target[i+1]){
+              swap(target[i], target[i+1]);
+              swapped = true;
+            }
+
+            this_thread::sleep_for(chrono::microseconds(delay));
+            if(!running) return;
+          }
+        }
+      }
+    }
+  };
+
   // Initialization stuff
   map<string, IAlgo*> algos;
   void reg(string name, IAlgo* func){
@@ -40,6 +76,7 @@ namespace algo {
   }
   void init(){
     reg("Bubble Sort", new BubbleSort());
+    reg("Cocktail Shaker Sort", new CocktailShakerSort());
   }
   void deinit(){
     for(pair<string, IAlgo*> a : algos)
