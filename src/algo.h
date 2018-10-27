01608de0dcccb46bd78274252e67835897a51930
trams <trams@itrams.me>
2018-10-28 01:38:38 +0200

    fixed everything

--- src/algo.h
+++ src/algo.h
@@ -5,14 +5,61 @@
 #include <string>
 #include <mutex>
 #include <map>
+#include <atomic>
+#include <iostream>
 
 using namespace std;
 
 namespace algo {
-  class IAlgo{
+  class InterruptedException : virtual public std::exception {};
+  class IAlgo {
   public:
     virtual ~IAlgo() {};
     virtual void run() = 0;
+  };
+
+  template <typename T>
+  struct TraceableAtom {
+    T _a;
+    mutable mutex mx;
+
+    operator T() {
+      lock_guard<mutex> lock(mx);
+      return _a;
+    }
+
+    TraceableAtom() : _a() {
+    }
+
+    TraceableAtom(const atomic<T>& a) {
+      _a = a.load();      
+    }
+
+    TraceableAtom(T& a) {
+      _a = a;      
+    }
+
+    TraceableAtom(const TraceableAtom& other) {
+      lock_guard<mutex> lock(other.mx);
+      _a = other._a;
+    }
+
+    TraceableAtom& operator=(const TraceableAtom& other){
+      T value;
+      {
+        lock_guard<mutex> lock(other.mx);
+        value = other._a;
+      }
+      lock_guard<mutex> lock(mx);
+      _a = value;
+      return *this;
+    }
+
+    TraceableAtom& operator=(T& other){
+      lock_guard<mutex> lock(mx);
+      _a = other;
+      return *this;
+    }
   };
 
   extern map<string, IAlgo*> algos;
@@ -20,6 +67,9 @@ namespace algo {
   void init();
   void deinit();
   void run(string name);
+  template <typename T>
+  void swap(T &a, T &b);
 }
 
 #endif
+
